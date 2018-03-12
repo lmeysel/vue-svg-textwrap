394bfbbe2abfb3a6e3ac9a4387ababa738294d4e
Ludwig Meysel <lmeysel@users.noreply.github.com>
2018-03-12 20:05:44 +0100

    added padding props to typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,18 +2,31 @@ interface ITextWrapperConfiguration {
 	/**
 	 * True, to set text-attribute as text instead of HTML.
 	 */
-	plain: boolean = false,
+	plain: boolean,
 	/**
 	 * The maximum width the text element can flow. Null to ignore width (i.e. does no wrapping).
 	 */
-	width: Number | null = null,
+	width: Number | null,
 	/**
 	 * The vertical alignment of the resulting textblock. Use 'none' to avoid touching the
 	 * text-element's transform-property. Otherwise any set value will be overwritten.
 	 */
-	align: 'top' | 'middle' | 'bottom' | 'baseline' | 'none' = 'none',
+	align: 'top' | 'middle' | 'bottom' | 'baseline' | 'none',
 	/**
 	 * The line height (i.e. will be passed to the dy-property of new-line tspan-elements).
 	 */
-	lineHeight: String | Number = '1.125em'
+	lineHeight: String | Number,
+
+	/** 
+	 * Sets padding for the left and the right side.
+	 */
+	padding: Number,
+	/**
+	 * Padding on the left side.
+	 */
+	paddingLeft: Number,
+	/**
+	 * Padding on the right side.
+	 */
+	paddingRight: Number,
 }
